2f57caee1ac1d80cb0faea863da3d2828644c558
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-02-28 16:27:25 -0500

    add more data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,18 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES
-('Ronald', 'Firbank', 1),
-('Virginia', 'Woolf', 1),
-('Piers', 'Gaveston', 0),
-('Charles', 'LeRoi', 1),
-('Katherine', 'Mansfield', 1),
-('Dora', 'Carrington', 0),
-('Edward', 'Bellamy', 0),
-('Montague', 'Summers', 1),
-('Octavia', 'Butler', 1),
-('Unica', 'Zurn', 1);
+-- INSERT INTO candidates (first_name, last_name, industry_connected)
+-- VALUES
+-- ('Ronald', 'Firbank', 1),
+-- ('Virginia', 'Woolf', 1),
+-- ('Piers', 'Gaveston', 0),
+-- ('Charles', 'LeRoi', 1),
+-- ('Katherine', 'Mansfield', 1),
+-- ('Dora', 'Carrington', 0),
+-- ('Edward', 'Bellamy', 0),
+-- ('Montague', 'Summers', 1),
+-- ('Octavia', 'Butler', 1),
+-- ('Unica', 'Zurn', 1);
+
+-- INSERT INTO parties (name, description)
+-- VALUES
+-- ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaS'),
+-- ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage'),
+-- ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have');
