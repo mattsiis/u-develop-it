5ad427476b4fab91b2531de4f51316c4cd08b55a
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-03-01 01:29:21 -0500

    connect two tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,21 @@
--- INSERT INTO candidates (first_name, last_name, industry_connected)
--- VALUES
--- ('Ronald', 'Firbank', 1),
--- ('Virginia', 'Woolf', 1),
--- ('Piers', 'Gaveston', 0),
--- ('Charles', 'LeRoi', 1),
--- ('Katherine', 'Mansfield', 1),
--- ('Dora', 'Carrington', 0),
--- ('Edward', 'Bellamy', 0),
--- ('Montague', 'Summers', 1),
--- ('Octavia', 'Butler', 1),
--- ('Unica', 'Zurn', 1);
+INSERT INTO parties (name, description)
+VALUES
+('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaS'),
+('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage'),
+('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have');
 
--- INSERT INTO parties (name, description)
--- VALUES
--- ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaS'),
--- ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage'),
--- ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have');
+
+
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+VALUES
+('Ronald', 'Firbank', 1, 1),
+('Virginia', 'Woolf', 1, 1),
+('Piers', 'Gaveston', 1, 0),
+('Charles', 'LeRoi', 2, 1),
+('Katherine', 'Mansfield', 2, 1),
+('Dora', 'Carrington', 3, 0),
+('Edward', 'Bellamy', 3, 0),
+('Montague', 'Summers', 3, 1),
+('Octavia', 'Butler', 3, 1),
+('Unica', 'Zurn', NULL, 1);
+
